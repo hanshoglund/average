7c375c68b67bd28d208a2bf3a9c6097dd64b3472
Evan Rutledge Borden <eborden@frontrowed.com>
2017-09-28 08:11:12 -0400

    Add quick check tests for type class instances.
    "distributive: c d v" is currently failing.
    
    Amended by Justus Sagemüller (leftaroundabout) to apply to
    the old list-based implementation.

---
+++ tests/Spec.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Main (main) where
+
+import Data.AdditiveGroup
+import Data.Monoid.Average
+import Data.VectorSpace
+import Test.Hspec
+import Test.Hspec.Checkers
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes (monoid)
+
+
+instance Arbitrary (Average Int) where
+  arbitrary = Average <$> arbitrary
+
+instance EqProp (Average Int) where
+  x =-= y = getAverage (fmap fromIntegral x :: Average Double) =-= getAverage (fmap fromIntegral y)
+
+main :: IO ()
+main =
+  hspec $
+    describe "Average" $ do
+      testBatch $ monoid (undefined :: Average Int)
+      describe "laws for: vector space" $ do
+        it "associativity" . property $ isAssoc @(Average Int) (^+^)
+        it "commutative" . property $ isCommut @(Average Int) (^+^)
+        it "left identity" . property $ leftId @(Average Int) (^+^) zeroV
+        it "right identity" . property $ rightId @(Average Int) (^+^) zeroV
+        describe "closure" $ do
+          it "distributive: c u v" . property $ \(c, u, v :: Average Int) ->
+            c *^ (u ^+^ v) =-= (c *^ u) ^+^ (c *^ v)
+          it "distributive: c d v" . property $ \(c, d, v :: Average Int) ->
+            (c ^+^ d) *^ v =-= c *^ v ^+^ d *^ v
+          it "associativity" . property $ \(c, d, v :: Average Int) ->
+            c *^ (d *^ v) =-= (c * d) *^ v
+          it "unitary" . property $ \(v :: Average Int) ->
+            1 *^ v =-= v
